bec031bb0e8949cb7759671cd7615af8ff13f83a
Mateus Melo Oliveira <Mateus99Melo@gmail.com>
2022-08-05 16:49:21 -0300

    Inicio do Projeto

---
+++ LiçãoIBM 05-08-2022/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace LiçãoIBM_05_08_2022 {
+    class Program {
+        static void Main(string[] args) {
+
+            int NumFunc = Convert.ToInt32(Console.ReadLine());
+            int QtdHoras = Convert.ToInt32(Console.ReadLine());
+            double ValorHora = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double Salario = (ValorHora * QtdHoras);
+
+            Console.WriteLine($"Number = {NumFunc}");
+            Console.WriteLine($"Salary = U$ {Salario.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
